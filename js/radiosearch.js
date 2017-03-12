cd0ccd0c464a96ec9497a82e100031f5b59685ab
guskant <gusni.kantu@gmail.com>
2017-03-12 16:15:58 +0000

    search

--- js/radiosearch.js
+++ js/radiosearch.js
@@ -37,6 +37,8 @@
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
     var idx = lunr(function () {
+            // use the language (ja)
+      this.use(lunr.jp);
       this.field('id');
       this.field('title', { boost: 10 });
       this.field('author');
