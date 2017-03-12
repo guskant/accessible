691b822b5d57f30039e5de79304428389103cc55
guskant <gusni.kantu@gmail.com>
2017-03-12 09:18:50 +0000

    radios

---
+++ radio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+grep "\- id:" ./_data/radios.yml | sed 's/- id://' > temp
+ALL=`cat temp | wc -l`
+L=1
+while [ $L -le $ALL ]; do
+  ID="`sed -n $L'p' temp`"
+  echo -e ---\\nlayout: muse\\nradio: $ID\\n--- > ./_radios/`echo $ID`.md
+#  L=`expr $L + 1`
+  L=$(( $L + 1 ))
+done
+rm temp
